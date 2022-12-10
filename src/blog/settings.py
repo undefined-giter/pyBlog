f90421cf327ace8ba79fe9b3852448d3033c1ea9
undefined-giter <75974108+undefined-giter@users.noreply.github.com>
2022-12-09 19:53:14 -0500

    Update settings.py

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blogadmin',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
